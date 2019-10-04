fa5ec7a0aca48bcf1168f6bdff7adb1a26da7de6
Joshua Weinstein <jweinstein@splunk.com>
2019-10-04 14:09:59 -0700

    initial changes

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from distutils.core import setup, Extension
 
-# A Python package may have multiple extensions, but this
-# template has one.
+
 module1 = Extension('DemoPackage',
                     define_macros = [('USE_PRINTER', '1')],
                     include_dirs = ['include'],
                     sources = ['src/demo.c'])
 
-setup (name = 'DemoPackage',
-       version = '1.0',
-       description = 'This is a demo package',
-       author = '<first> <last>',
-       author_email = 'person@site.com',
+setup (name = 'sword',
+       version = '0.0.1',
+       description = 'A data mining and searching C extension',
+       author = 'Joshua Weinstein',
+       author_email = 'jweinst1@berkeley.edu',
        url = 'https://docs.python.org/extending/building',
        long_description = open('README.md').read(),
        ext_modules = [module1])
