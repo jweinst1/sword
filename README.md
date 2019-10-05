44d5b024b8a201f1b2f62d551e600e191463ab2a
Joshua Weinstein <jweinstein@splunk.com>
2019-10-05 01:07:48 -0700

    sdist update

--- README.md
+++ README.md
@@ -5,4 +5,10 @@
 ## Intro
 
 `sword` is a Python C Extension for making data mining and data searching super fast. It's designed to 
-give you amazing performance in the comfort and simplicity of Python.
+give you amazing performance in the comfort and simplicity of Python. `sword` offers a wide array of tools
+to parse and search tabular data, csv, xml, and much more.
+
+### Status
+
+`sword` is still in the active development phase. It currently supports some CSV functionality. More detailed
+and rich features will be released in coming patch releases.
--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from distutils.core import setup, Extension
 
+classifers_list = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Operating System :: MacOS',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: Unix',
+    'Programming Language :: C',
+    'Topic :: Scientific/Engineering :: Information Analysis'
+    
+]
 
 sources_py = [
     'src/mainpy.c',
@@ -13,9 +22,11 @@ module1 = Extension('sword',
 
 setup (name = 'sword',
        version = '0.0.1',
+       license= 'MIT',
        description = 'A fast, sharp, Python C extension for data mining.',
        author = 'Joshua Weinstein',
        author_email = 'jweinst1@berkeley.edu',
        url = 'https://github.com/jweinst1/sword',
+       classifiers = classifers_list,
        long_description = open('README.md').read(),
        ext_modules = [module1])
